18051bef2fde473291d3b7861c1cc2efcb70e0d0
Jay Allen <jay.allen@nike.com>
2019-01-01 15:44:33 -0800

    fixed another intializer, simulation works in basic mode

--- monster-dice.py
+++ monster-dice.py
@@ -8,44 +8,56 @@ hero = ['hit', 'hit', 'run', 'run', '2hit', '2bra']
 heroin = ['hit', 'hit', 'bra', 'run', 'run', 'run']
 santa = ['hit', '2bra', 'helm', 'bra', 'ebar', 'run']
 
-bucket = [red, red, red, green, green, green, green, \
+
+
+def turn(number):
+    hits = 0
+    bras = 0
+    rolls = 0
+    runs = 0
+    ebar = 0
+    old_roll = []
+    bucket = [red, red, red, green, green, green, green, \
        green, green, yellow, yellow, yellow, hero, heroin, santa]
 
-hits = 0
-bras = 0
-rolls = 0
-runs = 0
-ebar = 0
-old_roll = []
+    while len(bucket) > 2 and hits < 3:
+        current_roll = []
+        sample_size = 3 - len(old_roll)
+        print("#old roll: ", len(old_roll), " sample_size: ", sample_size)
+        roll = random.sample(bucket, sample_size)
+        for item in roll:
+            bucket.remove(item)
+        current_roll = roll + old_roll
+        old_roll = []
+        print("#current roll: ", current_roll)
 
-while len(bucket) > 2 and hits < 3:
-    current_roll = []
-    sample_size = 3 - len(old_roll)
-    print("old roll: ", len(old_roll), " sample_size: ", sample_size)
-    roll = random.sample(bucket, sample_size)
-    current_roll = roll + old_roll
-    old_roll = []
-    print("current roll: ", current_roll)
+        for dice in current_roll:
+            dice_result = random.choice(dice)
+            print("#dice: ",dice_result)
+            if dice_result == 'hit':
+                hits += 1
+            if dice_result == '2hit':
+                hits += 2
+            if dice_result== 'run':
+                runs += 1
+                old_roll.append(dice)
+            if dice_result == 'bra':
+                bras += 1
+            if dice_result == '2bra':
+                bras += 2
+            if dice_result == 'helm':
+                bras -= 1
+            if dice_result == 'ebar':
+                ebar += 1
 
-    for dice in current_roll:
-        dice_result = random.choice(dice)
-        print("dice: ",dice_result)
-        bucket.remove(dice)
-        if dice_result == 'hit':
-            hits += 1
-        if dice_result == '2hit':
-            hits += 2
-        if dice_result== 'run':
-            runs += 1
-            old_roll.append(dice)
-        if dice_result == 'bra':
-            bras += 1
-        if dice_result == '2bra':
-            bras += 2
-        if dice_result == 'helm':
-            bras -= 1
-        if dice_result == 'ebar':
-            ebar += 1
+        rolls += 1
+        print("#Number: ", number, " Rolls: ", rolls, " Hits: ", hits, " Brains: ", bras)
+    return(number,rolls,hits,bras)
 
-    rolls += 1
-    print("Rolls: ", rolls, " Hits: ", hits, " Brains: ", bras)
+results = []
+
+for x in range(1, 10000):
+    results.append(turn(x))
+
+for result in results:
+    print(result)
